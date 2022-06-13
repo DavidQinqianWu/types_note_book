26d74829d3bca145e840817fbcea35484b1d6487
武秦乾 <wuqinqian@xiaoyezi.com>
2022-06-13 18:02:19 +0800

    完成isUnion

---
+++ IsUnion/index.ts
@@ -0,0 +1,14 @@
+// A extends A 这段看似没啥意义，主要是为了触发分布式条件类型，让 A 的每个类型单独传入。
+
+// [B] extends [A] 这样不直接写 B 就可以避免触发分布式条件类型，那么 B 就是整个联合类型。
+
+type IsUnion<A, B = A> = A extends A
+    ? [B] extends [A] // 这个是为了防止我们 传入单个类型, 单个类型需要返回false;
+        ? false
+        : true
+    : never;
+
+type IsUnionRes = IsUnion<1 | 2>;
+
+// 实例, 这里 [B] extends [A] 的意思, 即B整体这个变量属性是否等于A这个整体
+type IsUnionRes2 = IsUnion<1>;
